9bb64cd6f025606df3ca3e12b13108be1f2748d9
include07 <j.elfirqi@gmail.com>
2023-07-27 20:27:23 +0100

    -

---
+++ app.js
@@ -0,0 +1,25 @@
+// defining DOM elements
+
+const result = document.querySelector("#yes");
+
+const percentage = document.querySelector("#percentage");
+const ac = document.querySelector("#AC");
+const zeroNumber = document.querySelector("#zero");
+const oneNumber = document.querySelector("#one");
+const twoNumber = document.querySelector("#two");
+const threeNumber = document.querySelector("#three");
+const fourNumber = document.querySelector("#four");
+const fiveNumber = document.querySelector("#five");
+const sixNumber = document.querySelector("#six");
+const sevenNumber = document.querySelector("#seven");
+const eightNumber = document.querySelector("#eight");
+const nineNumber = document.querySelector("#nine");
+const divideButton = document.querySelector("#division");
+const multiplyButton = document.querySelector("#multiply");
+const minusButton = document.querySelector("#minus");
+const plusButton = document.querySelector("#plus");
+const equalButton = document.querySelector("#equal");
+const pointButton = document.querySelector("#point");
+
+// adding event listeners
+
